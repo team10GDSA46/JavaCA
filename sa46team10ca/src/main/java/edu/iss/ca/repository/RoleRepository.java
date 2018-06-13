f1ea420f30c05f606fd206ee6b8db8616d4397ee
Htet Wai Yan Htet Wai Yan <e0284026@u.nus.edu>
2018-06-13 18:44:22 +0800

    remove role repo

--- sa46team10ca/src/main/java/edu/iss/ca/repository/RoleRepository.java
+++ sa46team10ca/src/main/java/edu/iss/ca/repository/RoleRepository.java
@@ -1,14 +1,14 @@
-package edu.iss.ca.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
+//package edu.iss.ca.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
 //import edu.iss.ca.models.Role;
 
-public interface RoleRepository extends  JpaRepository<Role, Integer>{
+//public interface RoleRepository extends  JpaRepository<Role, Integer>{
 	
 /*	@Query("SELECT r.name FROM Role r")
 	ArrayList<String> findAllRolesNames();*/
 	
 /*	@Query("SELECT r FROM Role r WHERE r.name = :name")
 	ArrayList<Role> findRoleByName(@Param("name") String name);*/
-
-}
+//
+//}
