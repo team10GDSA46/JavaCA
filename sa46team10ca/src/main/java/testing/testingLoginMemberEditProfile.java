bd7d443e97172ae19839c9b34075e3e520f586cc
ngwingyipbianca <ngwingyi.pbianca@gmail.com>
2018-06-14 09:19:52 +0800

    final

---
+++ sa46team10ca/src/main/java/testing/testingLoginMemberEditProfile.java
@@ -0,0 +1,32 @@
+package testing;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class testingLoginMemberEditProfile {
+
+public static void main(String[] args) throws InterruptedException {
+	
+	System.setProperty("webdriver.gecko.driver","D:\\geckodriver.exe");
+	
+	WebDriver driver = new FirefoxDriver();
+	driver.get("http://localhost:8080/ca/");
+	driver.manage().window().maximize();
+	
+	driver.findElement(By.xpath("//*[@id=\"name\"]")).sendKeys("tonystark");
+	driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("memberpass");
+	
+	driver.findElement(By.xpath("//*[@id=\"submit\"]")).click();
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	Thread.sleep(3000);
+	
+	driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/div/ul/li[3]/a")).click();
+	
+	Thread.sleep(8000);
+	driver.quit();
+}
+}
